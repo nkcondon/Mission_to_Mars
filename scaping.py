684018ace605f91e4afbd2d137401c488aef260f
nkcondon <schnooze@hotmail.com>
2021-11-09 15:08:05 -0500

    add files

--- scaping.py
+++ scaping.py
@@ -29,7 +29,9 @@ def scrape_all():
       "news_paragraph": news_paragraph,
       "featured_image": featured_image(browser),
       "facts": mars_facts(),
-      "last_modified": dt.datetime.now()
+      "last_modified": dt.datetime.now(),
+      "hemispheres": hemisphere(browser) 
+
   }
 
   # Stop webdriver and return data
